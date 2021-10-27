f14e177bead758cb4c63986a3f50c7ce152fa303
omykhalska <83216292+omykhalska@users.noreply.github.com>
2021-10-27 21:40:22 +0300

    Добавляет решение задания №2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,8 +5,6 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
-
 const galleryContainer = document.querySelector('.gallery');
 
 galleryContainer.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+let currentTimeStored = 0;
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  currentTimeStored = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+  console.log(
+    `Мы остановили просмотр на ${Number.parseInt(currentTimeStored / 60)}мин:${
+      Number.parseInt(currentTimeStored) % 60
+    }сек. Продолжаем :-)`,
+  );
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(currentTimeStored);
+
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+}
