de15bcd416e0d34f760388bead26d4cb03f7f72f
omykhalska <83216292+omykhalska@users.noreply.github.com>
2021-10-28 09:51:56 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,8 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
 
 const galleryContainer = document.querySelector('.gallery');
 
